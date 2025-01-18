fe57d3dba6d08e6b473b71227fcc56e2a3a7d904
mmbb2w4r6y8i <844708246@qq.com>
2025-01-19 03:24:02 +0800

    场景切换界面

---
+++ Assets/Scripts/LevelGroup.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelGroup : MonoBehaviour
+{
+	public GameObject levelBtnPrefab;
+
+	private void Awake()
+	{
+		int sceneCount = SceneManager.sceneCountInBuildSettings;
+		for (int i = 1; i < sceneCount; i++)
+		{
+			Debug.Log(i);
+			var obj = Instantiate(levelBtnPrefab, transform);
+			obj.GetComponentInChildren<Text>().text = i.ToString();
+			var btn = obj.GetComponent<Button>();
+			int index = i;
+			btn.onClick.AddListener(() => { GameManager.Instance.SwitchScene(index); });
+		}
+	}
+}
