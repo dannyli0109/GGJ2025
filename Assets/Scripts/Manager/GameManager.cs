d251dd15ecfe1e0b60da83a7215adb7a4ea87d18
mmbb2w4r6y8i <844708246@qq.com>
2025-01-18 16:37:43 +0800

    增加BGM

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,5 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityUtils;
 
 public class GameManager : SingletonMono<GameManager>
 {
+	AudioSource bgmSource;
+	public List<AudioClip> bgmClips;
+
+	protected override void Awake()
+	{
+		base.Awake();
+		bgmSource = GetComponent<AudioSource>();
+	}
+
+	private void Start()
+	{
+		SwitchBgm(0);
+	}
+
+	public void SwitchScene(int id)
+	{
+		SceneManager.LoadScene(id);
+
+	}
+
+	public void SwitchBgm(int id)
+	{
+		if (id < bgmClips.Count)
+		{
+			bgmSource.clip = bgmClips[id];
+			bgmSource.Play();
+		}
+	}
 }
