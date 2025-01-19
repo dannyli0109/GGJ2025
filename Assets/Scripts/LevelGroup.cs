e1f560bc8ed28d079815c9876c65bd424bab7ec9
Danny Li <dannyli0109@gmail.com>
2025-01-19 13:47:49 +0800

    feat: 转场

--- Assets/Scripts/LevelGroup.cs
+++ Assets/Scripts/LevelGroup.cs
@@ -10,18 +10,18 @@ public class LevelGroup : MonoBehaviour
 
     public Sprite[] levelSprites;
 
-	private void Awake()
-	{
-		int sceneCount = SceneManager.sceneCountInBuildSettings;
-		for (int i = 1; i < sceneCount; i++)
-		{
-			Debug.Log(i);
-			var obj = Instantiate(levelBtnPrefab, transform);
-			//obj.GetComponentInChildren<Text>().text = i.ToString();
-			obj.GetComponent<Image>().sprite = levelSprites[(i - 1) % levelSprites.Length];
-			var btn = obj.GetComponent<Button>();
-			int index = i;
+    private void Awake()
+    {
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        for (int i = 1; i < sceneCount; i++)
+        {
+            Debug.Log(i);
+            var obj = Instantiate(levelBtnPrefab, transform);
+            //obj.GetComponentInChildren<Text>().text = i.ToString();
+            obj.GetComponent<Image>().sprite = levelSprites[(i - 1) % levelSprites.Length];
+            var btn = obj.GetComponent<Button>();
+            int index = i;
             btn.onClick.AddListener(() => { CircleTransition.Instance.TransitionToScene(index); });
-		}
-	}
+        }
+    }
 }
