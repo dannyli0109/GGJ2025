9107efc6f5e53e629f8a524e2258f9d7896fd0e3
Danny Li <dannyli0109@gmail.com>
2025-01-19 14:31:33 +0800

    feat: 复活tween

--- Assets/Scripts/GamePlay/MovementController.cs
+++ Assets/Scripts/GamePlay/MovementController.cs
@@ -1,189 +1,191 @@
 using System;
 using System.Collections;
+using HFSM;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Windows;
 
 public class MovementController : MonoBehaviour
 {
-	public PlayerMovementData movementData;
-	public ContactFilter2D groundContactFilter;
-	public Vector2 velocity => rig.velocity;
-	public bool jumpEnable => jumpCount > 0;
-	public bool sprintEnable = true;
-	[HideInInspector] public Vector3 spawnPos;
-	private int jumpCount;
-	public int JumpCount
-	{
-		get
-		{
-			return jumpCount;
-		}
-		set
-		{
-			jumpCount = Mathf.Max(0, value);
-		}
-	}
-	float landJumpSpeed;
-	//float airJumpSpeed;
+    public PlayerMovementData movementData;
+    public ContactFilter2D groundContactFilter;
+    public Vector2 velocity => rig.velocity;
+    public bool jumpEnable => jumpCount > 0;
+    public bool sprintEnable = true;
+    [HideInInspector] public Vector3 spawnPos;
+    private int jumpCount;
+    public int JumpCount
+    {
+        get
+        {
+            return jumpCount;
+        }
+        set
+        {
+            jumpCount = Mathf.Max(0, value);
+        }
+    }
+    float landJumpSpeed;
+    //float airJumpSpeed;
 
-	[HideInInspector] public bool inCoyoteTime = false;
-	[HideInInspector] public bool isOnGround => IsOnGround();
+    [HideInInspector] public bool inCoyoteTime = false;
+    [HideInInspector] public bool isOnGround => IsOnGround();
 
-	PlayerInput input;
-	BoxCollider2D col;
-	Rigidbody2D rig;
-	Camera mainCamera;
-	[HideInInspector] public GroundDetector groundDetector;
+    PlayerInput input;
+    BoxCollider2D col;
+    Rigidbody2D rig;
+    Camera mainCamera;
+    [HideInInspector] public GroundDetector groundDetector;
 
-	#region System Function
-	private void Awake()
-	{
-		input = GetComponent<PlayerInput>();
-		col = GetComponent<BoxCollider2D>();
-		rig = GetComponent<Rigidbody2D>();
-		groundDetector = GetComponentInChildren<GroundDetector>();
-		mainCamera = Camera.main;
-		landJumpSpeed = Mathf.Sqrt(2 * Mathf.Abs(Physics2D.gravity.y) * rig.gravityScale * movementData.landJumpHeight);
-		ResetJumpCount();
-		RecordSpawnPos();
-	}
+    #region System Function
+    private void Awake()
+    {
+        input = GetComponent<PlayerInput>();
+        col = GetComponent<BoxCollider2D>();
+        rig = GetComponent<Rigidbody2D>();
+        groundDetector = GetComponentInChildren<GroundDetector>();
+        mainCamera = Camera.main;
+        landJumpSpeed = Mathf.Sqrt(2 * Mathf.Abs(Physics2D.gravity.y) * rig.gravityScale * movementData.landJumpHeight);
+        ResetJumpCount();
+        RecordSpawnPos();
+    }
 
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		if (collision.CompareTag("Death"))
-		{
-			gameObject.transform.position = spawnPos;
-		}
-	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Death"))
+        {
+            gameObject.transform.position = spawnPos;
+            gameObject.GetComponent<PlayerFSM>().onDeath();
+        }
+    }
 
-	private void OnEnable()
-	{
-		//RecordSpawnPos();
-	}
+    private void OnEnable()
+    {
+        //RecordSpawnPos();
+    }
 
-	private void OnDisable()
-	{
-		transform.position = spawnPos;
-	}
-	#endregion
+    private void OnDisable()
+    {
+        transform.position = spawnPos;
+    }
+    #endregion
 
-	public void ResetJumpCount()
-	{
-		JumpCount = movementData.jumpCount;
-	}
+    public void ResetJumpCount()
+    {
+        JumpCount = movementData.jumpCount;
+    }
 
-	public void Jump()
-	{
-		float velocityX = rig.velocity.x;
-		//float velocityX = input.horizontal*movementData.speed;
-		//if (input.Move && input.horizontal*rig.velocity.x<0)
-		//{
-		//    velocityX *= -1;
-		//}
-		if (jumpCount == movementData.jumpCount)
-		{
-			SetVelocity(velocityX, landJumpSpeed);
-		}
-		//else
-		//{
-		//	if (rig.velocity.y > 0)
-		//	{
-		//		//上升过程
-		//		SetVelocity(velocityX, airJumpSpeed);
-		//	}
-		//	else
-		//	{
-		//		//下落过程
-		//		SetVelocity(velocityX, airJumpSpeed);
-		//	}
-		//}
-	}
+    public void Jump()
+    {
+        float velocityX = rig.velocity.x;
+        //float velocityX = input.horizontal*movementData.speed;
+        //if (input.Move && input.horizontal*rig.velocity.x<0)
+        //{
+        //    velocityX *= -1;
+        //}
+        if (jumpCount == movementData.jumpCount)
+        {
+            SetVelocity(velocityX, landJumpSpeed);
+        }
+        //else
+        //{
+        //	if (rig.velocity.y > 0)
+        //	{
+        //		//上升过程
+        //		SetVelocity(velocityX, airJumpSpeed);
+        //	}
+        //	else
+        //	{
+        //		//下落过程
+        //		SetVelocity(velocityX, airJumpSpeed);
+        //	}
+        //}
+    }
 
-	public void RecordSpawnPos()
-	{
-		spawnPos = transform.position;
-	}
+    public void RecordSpawnPos()
+    {
+        spawnPos = transform.position;
+    }
 
-	public void SetFallGravityScale()
-	{
-		rig.gravityScale = movementData.fallGravityScale;
-	}
+    public void SetFallGravityScale()
+    {
+        rig.gravityScale = movementData.fallGravityScale;
+    }
 
-	public void ResetGravityScale()
-	{
-		rig.gravityScale = 1;
-	}
+    public void ResetGravityScale()
+    {
+        rig.gravityScale = 1;
+    }
 
-	public void LimitFallSpeed()
-	{
-		if (rig.velocity.y < -movementData.maxFallSpeed)
-		{
-			rig.velocity = new Vector2(rig.velocity.x, -movementData.maxFallSpeed);
-		}
-	}
+    public void LimitFallSpeed()
+    {
+        if (rig.velocity.y < -movementData.maxFallSpeed)
+        {
+            rig.velocity = new Vector2(rig.velocity.x, -movementData.maxFallSpeed);
+        }
+    }
 
-	public void SetVelocityX(float v)
-	{
-		Vector2 velocity = rig.velocity;
-		velocity.x = v;
-		rig.velocity = velocity;
-	}
+    public void SetVelocityX(float v)
+    {
+        Vector2 velocity = rig.velocity;
+        velocity.x = v;
+        rig.velocity = velocity;
+    }
 
-	public void SetVelocityY(float v)
-	{
-		Vector2 velocity = rig.velocity;
-		velocity.y = v;
-		rig.velocity = velocity;
-	}
+    public void SetVelocityY(float v)
+    {
+        Vector2 velocity = rig.velocity;
+        velocity.y = v;
+        rig.velocity = velocity;
+    }
 
-	public void SetVelocity(float x, float y)
-	{
-		Vector2 v = new Vector2(x, y);
-		rig.velocity = v;
-	}
+    public void SetVelocity(float x, float y)
+    {
+        Vector2 v = new Vector2(x, y);
+        rig.velocity = v;
+    }
 
-	public void SetVelocity(Vector2 v)
-	{
-		rig.velocity = v;
-	}
+    public void SetVelocity(Vector2 v)
+    {
+        rig.velocity = v;
+    }
 
-	public void SetGravityScale(float scale)
-	{
-		rig.gravityScale = scale;
-	}
+    public void SetGravityScale(float scale)
+    {
+        rig.gravityScale = scale;
+    }
 
-	// 更新朝向
-	public void UpdateToward()
-	{
-		Vector3 scale = transform.localScale;
-		if (input.horizontal * scale.x < 0)
-		{
-			scale.x = Mathf.Sign(input.horizontal) * Mathf.Abs(scale.x);
-			transform.localScale = scale;
-		}
-	}
+    // 更新朝向
+    public void UpdateToward()
+    {
+        Vector3 scale = transform.localScale;
+        if (input.horizontal * scale.x < 0)
+        {
+            scale.x = Mathf.Sign(input.horizontal) * Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }
+    }
 
-	//public void SprintCountDown()
-	//{
-	//	StartCoroutine(SprintCountDownProgress(movementData.sprintCD));
-	//}
+    //public void SprintCountDown()
+    //{
+    //	StartCoroutine(SprintCountDownProgress(movementData.sprintCD));
+    //}
 
-	public bool IsOnGround()
-	{
-		return rig.IsTouching(groundContactFilter);
-	}
+    public bool IsOnGround()
+    {
+        return rig.IsTouching(groundContactFilter);
+    }
 
-	IEnumerator SprintCountDownProgress(float t)
-	{
-		sprintEnable = false;
-		yield return new WaitForSeconds(t);
-		sprintEnable = true;
-	}
+    IEnumerator SprintCountDownProgress(float t)
+    {
+        sprintEnable = false;
+        yield return new WaitForSeconds(t);
+        sprintEnable = true;
+    }
 
-	IEnumerator DelayFunc(UnityAction action, float t)
-	{
-		yield return new WaitForSeconds(t);
-		action();
-	}
+    IEnumerator DelayFunc(UnityAction action, float t)
+    {
+        yield return new WaitForSeconds(t);
+        action();
+    }
 }
